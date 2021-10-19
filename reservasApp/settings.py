1aa5867f95a45e7a9e7e3cbcf3f80edb6d8efeb6
Jerson Perez <ing.jersonperez@gmail.com>
2021-10-19 18:06:55 -0500

    [Update] imporve security

--- reservasApp/settings.py
+++ reservasApp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^#3wwsdam&e)jp@xx+a=wa$34ulp3hxu=)x(wqcg!q-y%onksx'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
